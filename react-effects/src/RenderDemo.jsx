6ef2a12892c8716bb25f4384b9fdbb4e86362e32
simo1213 <127089347+simo1213@users.noreply.github.com>
2025-08-14 18:36:04 -0500

    Add files via upload

---
+++ react-effects/src/RenderDemo.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const rand = () => Math.floor(Math.random() * 1000);
+const log = console.log;
+
+function RenderDemo() {
+  const [a, setA] = useState(rand());
+  const [b, setB] = useState(rand());
+  log("rendering a=", a, "b=", b);
+
+  function change1() {
+    log("change1 a=", a);
+    setA(rand());
+    log("  change1 end a=", a);
+  }
+
+  function change2() {
+    log("change2 b=", b);
+    setB(rand());
+    log("  change2 end b=", b);
+  }
+
+  return (
+    <div>
+      <button onClick={change1}>{a}</button>
+      <button onClick={change2}>{b}</button>
+    </div>
+  );
+}
+
+export default RenderDemo;
