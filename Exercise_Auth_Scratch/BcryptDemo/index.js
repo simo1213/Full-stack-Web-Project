4f037e1fd678134e8d3c167e65d1a6b42a2a93d8
simo1213 <127089347+simo1213@users.noreply.github.com>
2025-08-14 18:05:49 -0500

    Add files via upload

---
+++ Exercise_Auth_Scratch/BcryptDemo/index.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (pw) => {
+    const hash = await bcrypt.hash(pw, 12);
+    console.log(hash);
+}
+
+const login = async (pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+    if (result) {
+        console.log("LOGGED YOU IN! SUCCESSFUL MATCH!")
+    } else {
+        console.log("INCORRECT!")
+    }
+}
+
+hashPassword('monkey');
+login('monkey', '$2b$12$YS9GdWUznoM7r1522knuY.1dq1taWra5zgG7N1WzHs4j.fridopWS')
